49c73582bc75f9b1e8734aa3a364502f18f13618
michal097 <slusarczyk.michal23@gmail.com>
2020-12-23 13:43:57 +0100

    add tin backend

--- src/test/java/com/tinFInale/JavPy.java
+++ src/test/java/com/tinFInale/JavPy.java
@@ -1,6 +1,9 @@
 package com.tinFInale;
 
 import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
 
 public class JavPy {
     public static void main(String[] args) {
@@ -15,7 +18,26 @@ public class JavPy {
             Arrays.stream(s)
                     .reduce((a,b)->a+", " + b)
                     .ifPresent(System.out::println);
+
+            System.out.println(param(5));
         }
-        
+
+    IntStream.rangeClosed(0,100)
+             .mapToObj(x-> x%3==0 ? (x% 5 == 0 ? "FizzBuzz" : "Fizz"): (x%5==0)?"Buzz": "")
+             .filter(x-> !x.equals(""))
+             .forEach(System.out::println);
+        function(5);
+
+    }
+
+
+    public static boolean param(Integer param){
+        Predicate<Integer> pred = x-> x< 5;
+        return pred.test(param);
+    }
+    public static void function(Integer a){
+        Function<Integer,Double> fun = b->b/2.0;
+        System.out.println(fun.apply(a).doubleValue());
+
     }
 }
